0d39ac645e2364d40b2bda7d50a5e8acdc22919e
hyunlee328 <78284423+hyunlee328@users.noreply.github.com>
2023-04-26 19:24:00 -0700

    backend

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+
+    private int to_square;
+    public Program(int to_be_squared)
+    {
+        to_square = to_be_squared;
+    }
+
+    public int square()
+    {
+        return to_square * to_square;   
+    }
+}
+    
