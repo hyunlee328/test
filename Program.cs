a21dfad42581112ef98f96c8e1569f59ad5d4023
hyunlee328 <78284423+hyunlee328@users.noreply.github.com>
2023-04-26 19:27:54 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+public static class Program
+{
+    public static int square(int to_square)
+    {
+        return to_square * to_square;
+    }
+}
+    
